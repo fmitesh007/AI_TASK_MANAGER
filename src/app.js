06346138d5413d6cec4df75d45c49000e86709d6
Mitesh Firake <40400158+fmitesh007@users.noreply.github.com>
2026-03-24 17:42:06 +0530

    fix: fixed PORT issues

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const userRouter = require("./routes/userRoutes.js");
 const taskRouter = require("./routes/taskRoutes.js");
 const { connectDB } = require("./db/db.js");
 const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 8000;
 require("dotenv").config();
 connectDB();
 app.use(express.json());
@@ -15,6 +16,6 @@ app.get("/", (req, res) => {
 app.use("/api", userRouter);
 app.use("/api", taskRouter);
 
-app.listen(8000, () => {
-  console.log("server running at 8000");
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
 });
